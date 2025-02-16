68051b10937e0ef193bdf8263ea1bb634ee41e51
keyboard-slayer <git@keyb.moe>
2025-02-16 22:14:29 +0100

    end and begining of the line the helix way

--- lua/config/bindings.lua
+++ lua/config/bindings.lua
@@ -5,6 +5,8 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 vim.keymap.set("n", "<leader>b", "<cmd>GitBlameToggle<CR>")
+vim.keymap.set("n", "gh", "^")
+vim.keymap.set("n", "gl", "$")
 vim.keymap.set("n", "-", "<cmd>Oil<CR>")
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p')
