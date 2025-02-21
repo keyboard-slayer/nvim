70170c44c219bf5785a8fc4ce9419b54d8baccd4
keyboard-slayer <git@keyb.moe>
2025-02-21 22:13:02 +0100

    make diag msg visible

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,13 +19,29 @@ vim.o.splitright = true
 vim.o.splitbelow = true
 -- vim.o.list = true
 vim.o.inccommand = "nosplit"
--- vim.o.cursorline = true
+vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.laststatus = 3
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = true,
+		header = "",
+		prefix = "",
+	},
+})
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
